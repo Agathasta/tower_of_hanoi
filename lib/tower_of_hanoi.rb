f4f441a77e5cef1110c755120f0ef599b8b453de
Agathasta <69153207+Agathasta@users.noreply.github.com>
2020-12-21 12:53:28 +0100

    Add color

--- lib/tower_of_hanoi.rb
+++ lib/tower_of_hanoi.rb
@@ -10,20 +10,22 @@ class Game
   end
 
   def intro
-    puts
-    puts '***** TOWER OF HANOI *****'
-    puts 'The objective of the puzzle is to move all disks to another rod,'
-    puts 'obeying the following simple rules:'
-    puts '1. Only one disk can be moved at a time.'
-    puts '2. No larger disk may be placed on top of a smaller disk.'
+    puts `clear`
+    puts "\e[40m***** TOWER OF HANOI *****\e[0m"
+    puts <<~INTRO
+    \nThe objective of the puzzle is to move all disks to another rod obeying the following simple rules:
+      1. Only one disk can be moved at a time.
+      2. No larger disk may be placed on top of a smaller disk.
+    INTRO
   end
 
   def intro_moves
-    puts 'To move the disks, input two numbers separated by a space:'
-    puts    'the first one is the rod you take the disk FROM,'
-    puts    'the second is the rod you move the disk TO.'
-    puts 'V.gr.: 1 3 will move the disk from rod 1 to rod 3.'
-    puts
+    puts <<~INSTRUCTIONS
+      \nTo move the disks, input two numbers separated by a space:
+        - the first one is the rod you take the disk FROM,
+        - the second is the rod you move the disk TO.
+      \e[37mV.gr.: 1 3 will move the disk from rod 1 to rod 3.\e[0m
+    INSTRUCTIONS
   end
 
   def play
@@ -32,7 +34,7 @@ class Game
       @board.new_board
       @board.display
     end
-    puts 'CONGRATULATIONS!!'
+    puts "\e[32mCONGRATULATIONS!!\e[0m"
     puts
   end
 end
